f3a080e19285402130589291c91e4482c5a3f208
Bardizba Z <moch.bardizba@gmail.com>
2022-06-18 00:59:21 +0700

    feat: list contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,22 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css';
 
-const Contact = () => {
+const Contact = ({ data : {name, phone, email, photo} }) => {
     return (
-        <></>
+        <div className='contact'>
+            <div className='left'>
+                <img src={photo} alt={name} />
+            </div>
+            <div className='right'>
+                <div>
+                    <h2>{name}</h2>
+                    <p>{phone}</p>
+                    <p>{email}</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
